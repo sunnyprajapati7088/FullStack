2397725e7b902f80b6676e4a2992d1f2fdbcbcde
sunny prajapati <sunnyprajapati9761@gmail.com>
2024-12-11 19:57:16 +0530

    2

--- src/components/Map/MapConfig.ts
+++
@@ -1,9 +0,0 @@
-import { Icon } from 'leaflet';
-
-// Fix for default marker icon
-delete (Icon.Default.prototype as any)._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-});
